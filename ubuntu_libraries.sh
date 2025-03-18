0a290586b83939df7b05e35b1b7d6247a2e64caa
e-perl-NOAA <elizabeth.gugliotti@noaa.gov>
2025-03-18 16:44:15 +0000

    renv and bash

---
+++ ubuntu_libraries.sh
@@ -0,0 +1,14 @@
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libfontconfig1-dev
+sudo apt-get install -y libharfbuzz-dev libfribidi-dev
+sudo apt-get update
+sudo apt-get install --only-upgrade libstdc++6
+sudo apt-get install -y libxml2-dev
+sudo apt-get install -y libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
+sudo apt-get install gfortran
+sudo apt-get install liblapack-dev libblas-dev
+sudo apt install libgit2-dev libx11-dev pandoc
+sudo apt install -y libudunits2-dev libproj-dev libgdal-dev
+sudo apt install cmake gdal-bin libgdal-dev libudunits2-dev
+sudo apt install cmake
+sudo apt install libgit2-dev
