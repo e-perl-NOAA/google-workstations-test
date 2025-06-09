8c95ff476200d4d4348c6305cc0a9b2bb5b699a2
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2025-06-09 10:38:34 -0400

    Update ubuntu_libraries.sh

--- ubuntu_libraries.sh
+++ ubuntu_libraries.sh
@@ -9,12 +9,11 @@ sudo apt-get install gfortran
 sudo apt-get install liblapack-dev libblas-dev
 sudo apt install libgit2-dev libx11-dev pandoc
 sudo apt install -y libudunits2-dev libproj-dev libgdal-dev
-sudo apt install cmake gdal-bin libgdal-dev libudunits2-dev
-sudo apt install cmake
-sudo apt install libgit2-dev
+sudo apt install -y cmake gdal-bin libgdal-dev libudunits2-dev
 sudo apt-get update
 sudo apt install -y software-properties-common
 sudo apt update
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install -y --only-upgrade libstdc++6
